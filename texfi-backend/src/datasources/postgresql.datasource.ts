28254c6506837c61ceb52968a682ec3c0557507d
unknown <harangomez@gmail.com>
2025-10-04 01:49:17 -0400

    Actualizacion de datasource para despliegue3

--- texfi-backend/src/datasources/postgresql.datasource.ts
+++ texfi-backend/src/datasources/postgresql.datasource.ts
@@ -4,13 +4,9 @@ import {juggler} from '@loopback/repository';
 const config = {
   name: "postgresql",
   connector: "postgresql",
-  url: "${DATABASE_URL}",
+  url: process.env.DATABASE_URL, // ✅ CAMBIA ESTA LÍNEA
 };
 
-// Observe application's life cycle to disconnect the datasource when
-// application is stopped. This allows the application to be shut down
-// gracefully. The `stop()` method is inherited from `juggler.DataSource`.
-// Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
 @lifeCycleObserver('datasource')
 export class PostgresqlDataSource extends juggler.DataSource implements LifeCycleObserver {
   static dataSourceName = 'postgresql';
