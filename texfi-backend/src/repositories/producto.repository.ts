4cebe6393bd34a88469dd614729bc177ed5511aa
unknown <harangomez@gmail.com>
2025-10-11 06:42:29 -0400

    Ajuste en producto.repository para mejorar los get y poder llamar el nombre del cliente

--- texfi-backend/src/repositories/producto.repository.ts
+++ texfi-backend/src/repositories/producto.repository.ts
@@ -1,4 +1,4 @@
-import {inject} from '@loopback/core';
+/*import {inject} from '@loopback/core';
 import {DefaultCrudRepository} from '@loopback/repository';
 import {PostgresqlDataSource} from '../datasources';
 import {Producto, ProductoRelations} from '../models';
@@ -6,5 +6,25 @@ import {Producto, ProductoRelations} from '../models';
 export class ProductoRepository extends DefaultCrudRepository<Producto, typeof Producto.prototype.id, ProductoRelations> {
   constructor(@inject('datasources.postgresql') dataSource: PostgresqlDataSource) {
     super(Producto, dataSource);
+  }
+}*///Si la modificacion no funciona reactivar este codigo y desactivar el siguiente
+
+import {inject, Getter} from '@loopback/core';
+import {DefaultCrudRepository, repository, BelongsToAccessor} from '@loopback/repository';
+import {PostgresqlDataSource} from '../datasources';
+import {Producto, ProductoRelations, Cliente} from '../models';
+import {ClienteRepository} from './cliente.repository';
+
+export class ProductoRepository extends DefaultCrudRepository<Producto, typeof Producto.prototype.id, ProductoRelations> {
+  public readonly cliente: BelongsToAccessor<Cliente, typeof Producto.prototype.id>;
+
+  constructor(
+    @inject('datasources.postgresql') dataSource: PostgresqlDataSource,
+    @repository.getter('ClienteRepository')
+    protected clienteRepositoryGetter: Getter<ClienteRepository>,
+  ) {
+    super(Producto, dataSource);
+    this.cliente = this.createBelongsToAccessorFor('cliente', clienteRepositoryGetter);
+    this.registerInclusionResolver('cliente', this.cliente.inclusionResolver);
   }
 }
